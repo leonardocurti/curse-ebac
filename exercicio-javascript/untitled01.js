80f8aeaba4c163f535d52dd78195778d64265b6a
leonardocurti <32382067+leonardocurti@users.noreply.github.com>
2024-02-28 19:51:16 -0300

    segundo comit

--- exercicio-javascript/untitled01.js
+++ exercicio-javascript/untitled01.js
@@ -1,43 +1,33 @@
-const form = document.getElementById('form-deposito');
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
+const form = window.document.getElementById('form');
+const saldo = window.document.getElementById('saldo');
+const saque = window.document.getElementById('saque');
+const acerto = document.getElementById('saquerealizado');
+const erro = document.getElementById('erro')
 
-function validaNome(nomeCompleto) {
-    const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length >= 2;
+
+
+function sacandoDinheiro(saldo, saque){
+    return saldo > saque;
 }
 
-form.addEventListener('submit', function(e) {
-    let formEValido = false;
+form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const numeroContaBeneficiario = document.getElementById('numero-conta');
-    const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
-
-    formEValido = validaNome(nomeBeneficiario.value)
-    if (formEValido) {
-        const containerMenssagemSucesso = document.querySelector('.sucess-message');
-        document.querySelector('.sucess-message').innerHTML = mensagemSucesso;
-        containerMenssagemSucesso.style.display = 'block';
-
-        nomeBeneficiario.value = '';
-        numeroContaBeneficiario.value = '';
-        valorDeposito.value = '';
-    } else {
-        nomeBeneficiario.style.border = '1px solid red'
-        document.querySelector('.error-message').style.display = 'block'
-    }
+    let validarForm = sacandoDinheiro(saldo.valueAsNumber, saque.valueAsNumber);
+    if (validarForm){
+        acerto.style.display = 'block';
+        saldo.value='';
+        saque.value='';
+    } 
 })
 
-nomeBeneficiario.addEventListener('keyup', function(e){
-    console.log(e.target.value);
-    formEValido = validaNome(e.target.value);
-
-        if (!formEValido) {
-        nomeBeneficiario.classList.add('error')
-        document.querySelector('.error-message').style.display = 'block'
+saque. addEventListener('keyup', function(e){
+    let validarForm = sacandoDinheiro(saldo.valueAsNumber, saque.valueAsNumber);
+    if (!validarForm){
+        erro.style.display = 'block'
+        acerto.style.display = 'none'
     } else {
-        nomeBeneficiario.classList.remove('error')
-        document.querySelector('.error-message').style.display = 'none'
+        erro.style.display = 'none'
     }
+
 })
